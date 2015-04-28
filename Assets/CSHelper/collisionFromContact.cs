6b9ce59dc175a39a20787ef0ef45f334134f4b7d
Hesk <jobhesk@gmail.com>
2015-04-29 01:03:44 +0800

    up
    up

---
+++ Assets/CSHelper/collisionFromContact.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(AudioSource))]
+public class collisionFromContact : MonoBehaviour
+{
+
+	normalwalk2 player;
+	AudioSource asr;
+	Transform t;
+	// Use this for initialization
+	void Start ()
+	{
+		t = GetComponent<Transform> ();
+		asr = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+
+	}
+	
+	void OnCollisionEnter (Collision collision)
+	{
+		if (collision.gameObject.CompareTag ("Player")) {
+			foreach (ContactPoint contact in collision.contacts) {
+				Debug.DrawRay (contact.point, contact.normal, Color.white);
+				player = collision.gameObject.GetComponent<normalwalk2> ();
+				player.exploreFrom (contact.point, contact.normal, 10f);
+			}
+			if (collision.relativeVelocity.magnitude > 0.2f) {
+				asr.Play ();
+			}
+		}
+	}
+
+}
