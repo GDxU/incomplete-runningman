6cab09dbd7503835ba5f062ec21fcae0b184d633
Hesk <jobhesk@gmail.com>
2015-05-24 02:37:39 +0800

    update
    yes

--- Assets/CSHelper/RezzerOnSphere.cs
+++ Assets/CSHelper/RezzerOnSphere.cs
@@ -11,79 +11,42 @@ public class itemConf
 	public bool justOnSurface = false;
 }
 
-public class RezzerOnSphere : MonoBehaviour
+public class RezzerOnSphere : resBase
 {  
 
 	public float angleControl = 90F, minDistance, maxDistance, surfaceDistanceRadius;
 	//public int objectCountMin, objectCountMax;
 	[SerializeField]
 	public List<itemConf>
-		fab = new List<itemConf> (1);
-
+		randomLocatedObjects = new List<itemConf> (1);
 
 	// Use this for initialization
-	void Start ()
+	protected void Start ()
 	{
-//		if (objectCountMin > objectCountMax) {
-//			Debug.LogError ("objectCountMin needs to be smaller than objectCountMax");
-//			return;
-//		}
-//		if (objectCountMax < 1) {
-//			Debug.LogError ("objectCountMax needs to be bigger than zero;");
-//			return;
-//		}
-		if(surfaceDistanceRadius == null){
+		if (surfaceDistanceRadius == null) {
 			Debug.LogError ("surfaceDistanceRadius needs to be bigger than zero;");
 			return;
 		}
-		for (int h = 0; h<fab.Count; h++) {
-			itemConf item = fab [h];
+		base.Start ();
+	}
+
+	protected override void init ()
+	{
+		if (surfaceDistanceRadius == null) {
+			Debug.LogError ("surfaceDistanceRadius needs to be bigger than zero;");
+			return;
+		}
+		for (int h = 0; h<randomLocatedObjects.Count; h++) {
+			
+			itemConf item = randomLocatedObjects [h];
+			
+			float distance_object_from_center = 
+				item.justOnSurface ? surfaceDistanceRadius : Random.Range (minDistance, maxDistance);
+			
 			for (int i = 0; i<item.randomCounts; i++) {
-				rez (item.itemPrefab, item.justOnSurface);
+				rezIndependent (item.itemPrefab, distance_object_from_center);
 			}
 		}
 	}
 
-	protected void rez (GameObject prefab, bool on)
-	{
-		Vector3 V = GenerateRandomPos (on);
-		Quaternion rot = Quaternion.Euler (getNormal (V)); 
-		GameObject openobject = Instantiate (prefab, V, Quaternion.identity) as GameObject;
-		openobject.transform.rotation = Quaternion.FromToRotation (-openobject.transform.up, getNormal (V)) * openobject.transform.rotation;
-		openobject.transform.parent = GameObject.FindGameObjectWithTag ("Ground").transform;
-	}
-
-	public Vector3 getNormal (Vector3 vpos)
-	{
-		Vector3 t = GetComponent<Transform> ().position - vpos;
-		float distance = t.magnitude;
-		return t / distance;
-	}
-	// Update is called once per frame
-	void Update ()
-	{
-
-   
-
-	}
-
-	public Vector3 GenerateRandomPos (bool onSurface)
-	{
-
-		float onSurfaceDistance = onSurface ? surfaceDistanceRadius : Random.Range (minDistance, maxDistance);
-		Vector3 newPos = Random.onUnitSphere * onSurfaceDistance;
-		// newPos.y = originalPos.y;
-		return newPos;
-	}
-
-	public Vector3 GetPointOnUnitSphereCap (Quaternion targetDirection, float angle)
-	{
-		float angleInRad = Random.Range (0.0F, angle) * Mathf.Rad2Deg;
-		Vector3 pointoncircle = (Random.insideUnitCircle.normalized) * Mathf.Sin (angleInRad);
-		float distance = Mathf.Tan (angleInRad);
-		Vector2 PointOnCircle = Random.insideUnitCircle;
-		//Mathf.Cos(angleInRad)
-		Vector3 V = new Vector3 (PointOnCircle.x, PointOnCircle.y, distance);
-		return targetDirection * V;
-	}
 }
---
+++ Assets/CSHelper/resBase.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+
+public abstract class resBase : MonoBehaviour
+{
+	public bool autoInit = false;
+
+	protected abstract void init ();
+
+	public virtual void Start ()
+	{
+		if (autoInit)
+			init ();
+	}
+
+	#region making and locating the objects
+	protected virtual void createobjectsByConnectionsOnSphere (Vector3 position, Quaternion lookat, int i)
+	{
+		
+	}
+
+	protected virtual void createobjectsByConnectionsOnSphere (Vector3 position, Quaternion lookat)
+	{
+		
+	}
+
+	protected virtual void createobjectsByConnectionsOnSphere (Vector3 position)
+	{
+		
+	}
+	
+	protected void initByConnectionsOnSphere (Vector3 start, float angleLimit, int count, float sphere_radius_distance)
+	{
+		Vector3 m = start;
+		for (int j=0; j<count; j++) {
+			m = fromOnSphereNowToRandomNewPositionInDistance (angleLimit, m, sphere_radius_distance);
+			Quaternion lookat = Quaternion.FromToRotation (Vector3.up, getNormal (m)) * Quaternion.identity;
+			createobjectsByConnectionsOnSphere (m, lookat, j);
+			createobjectsByConnectionsOnSphere (m, lookat);
+			createobjectsByConnectionsOnSphere (m);
+		}
+	}
+	#endregion
+
+
+	protected void rezIndependent (GameObject prefab, float distance)
+	{
+		Vector3 V = GenerateRandomPos (distance);
+		Quaternion rot = Quaternion.Euler (getNormal (V)); 
+		GameObject openobject = Instantiate (prefab, V, Quaternion.identity) as GameObject;
+		openobject.transform.rotation = Quaternion.FromToRotation (-openobject.transform.up, getNormal (V)) * openobject.transform.rotation;
+		openobject.transform.parent = GameObject.FindGameObjectWithTag ("Ground").transform;
+	}
+
+
+	/**
+	 * to calculate the position from now to another new point position
+	 */
+	protected Vector3 fromOnSphereNowToRandomNewPositionInDistance (float angleDetla, Vector3 nowPos, float distance_radius)
+	{
+		return GetPoint2 (genPositionfromQuaternion (nowPos), angleDetla) * distance_radius;
+	}
+
+	/**
+	 * get the normal angle by position
+	 */
+	public Vector3 getNormal (Vector3 vpos)
+	{
+		Vector3 t = GetComponent<Transform> ().position - vpos;
+		float distance = t.magnitude;
+		return t / distance;
+	}
+
+	public Quaternion genPositionfromQuaternion (Vector3 normalized)
+	{
+		return  Quaternion.LookRotation (normalized);
+	}
+
+	public Vector3 GenerateRandomPos (float onSurfaceDistance)
+	{
+		//float onSurfaceDistance = onSurface ? surfaceDistanceRadius : Random.Range (minDistance, maxDistance);
+		Vector3 newPos = Random.onUnitSphere * onSurfaceDistance;
+		return newPos;
+	}
+
+	/**
+	 * http://answers.unity3d.com/questions/58692/randomonunitsphere-but-within-a-defined-range.html?sort=oldest
+	 * 
+	 * I'm trying to instiate a prefabs randomly onUnitSphere, but Instead of instantiating all over the sphere I want them to only instantiate within a small radias on the sphere.
+	 */
+	public static Vector3 GetPointOnUnitSphereCap (Quaternion targetDirection, float angle)
+	{
+		float angleInRad = Random.Range (0.0F, angle) * Mathf.Rad2Deg;
+		Vector3 pointoncircle = (Random.insideUnitCircle.normalized) * Mathf.Sin (angleInRad);
+		float distance = Mathf.Tan (angleInRad);
+		Vector2 PointOnCircle = Random.insideUnitCircle;
+		//Mathf.Cos(angleInRad)
+		Vector3 V = new Vector3 (PointOnCircle.x, PointOnCircle.y, distance);
+		return targetDirection * V;
+	}
+
+	public static Vector3 GetPoint2 (Quaternion targetDirection, float angle)
+	{
+		double angleInRad = Mathf.Clamp (90.0f - angle, Mathf.Epsilon, 90.0f - Mathf.Epsilon) * Mathf.Deg2Rad;
+		float distance = Mathf.Tan ((float) angleInRad);
+		Vector3 PointOnCircle = Random.insideUnitCircle;
+		Vector3 V = new Vector3 (PointOnCircle.x, PointOnCircle.y, distance);
+
+		return targetDirection * V;
+
+	}
+}
